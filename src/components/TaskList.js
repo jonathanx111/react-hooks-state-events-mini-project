210cdc1a532736576311d6f19954baa0d7ae3561
jonathanx111 <jonathanx111@gmail.com>
2021-01-13 20:13:53 -0500

    commit tree ffafbaa5a8e54a33f11729ca383cff0c96d85d67

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ onDeleteTask, tasks }) {
+  const tasksList = tasks.map(task => {
+    return <Task 
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      onDeleteTask={onDeleteTask} />
+  })
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksList}
     </div>
   );
 }
